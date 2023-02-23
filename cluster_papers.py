dc400febdd67944c3b30f01794ed0eefce4722f7
twktheainur <andon.tchechmedjiev@mines-ales.fr>
2023-02-23 16:05:18 +0100

    Added independent script to generate a clustering

---
+++ cluster_papers.py
@@ -0,0 +1,111 @@
+from argparse import ArgumentParser
+import itertools
+import json
+import sys
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
+
+import matplotlib.pyplot as plt
+
+
+from tqdm import tqdm, trange
+import numpy as np
+
+
+parser = ArgumentParser("Embed paper titles and abstracts.")
+
+parser.add_argument(
+    "input_papers",
+    nargs=1,
+    describe="Input json file resulting from SemanticScholar extraction")
+
+parser.add_argument(
+    "input_embeddings",
+    nargs=1,
+    describe="Input npy embedding file")
+
+parser.add_argument(
+    "input_scores",
+    nargs=1,
+    describe="Input npy with paper scores file")
+
+parser.add_argument("--dim_red",
+                    "-dr",
+                    nargs=1,
+                    default=["isomap"],
+                    choices=["mds", "lle", "isomap", "t-sne"],
+                    describe="The dimension reduction technique to use. Default: isomap.")
+
+parser.add_argument("--num_dim",
+                    "-nd",
+                    nargs=1,
+                    type=int,
+                    default=[3],
+                    describe="The number of dimensions. Default: 3")
+
+parser.add_argument("--clustering",
+                    "-c",
+                    nargs=1,
+                    default=["agglo"],
+                    choices=["kmeans", "agglo", "spectral", "dbscan"],
+                    describe="The clustering method to use. Default: agglo")
+
+parser.add_argument("--num_clusters", "-nc", nargs=1, type=int, default=[5])
+
+
+if __name__ == "__main__":
+    args = parser.parse_args(sys.argv)
+
+    with open(args.input_papers[0]) as f:
+        papers = json.load(f)
+
+    embedding = np.load(args.input_embeddings[0])
+
+    scores = np.load(args.input_scores[0])
+
+    connectivity = kneighbors_graph(
+        embedding, n_neighbors=50, include_self=False
+    )
+    # make connectivity symmetric
+    # connectivity = 0.5 * (connectivity + connectivity.T)
+
+    clustering = AgglomerativeClustering(
+        n_clusters=args.num_clusters[0], linkage="ward", connectivity=connectivity
+    ).fit(embedding)
+
+    print(np.unique(clustering.labels_, return_counts=True))
+
+    from sklearn.manifold import LocallyLinearEmbedding, Isomap, TSNE, MDS
+
+    n_components = args.num_dim[0]
+    if args.dim_red[0] == "isomap":
+        red = Isomap(n_components=n_components)
+    elif args.dim_red[0] == "t-sne":
+        red = TSNE(n_components=n_components)
+    elif args.dim_red[0] == "lle":
+        red = LocallyLinearEmbedding(n_components=n_components)
+    elif args.dim_red[0] == "mds":
+        red = MDS(n_components=2)
+
+    matrix_2D = red.fit_transform(embedding)
+
+    pairs = itertools.product(range(n_components), repeat=2)
+    for pair in pairs:
+        if pair[0] != pair[1]:
+            fig = plt.figure(dpi=300)
+            plt.scatter(matrix_2D.T[pair[0]],
+                        matrix_2D.T[pair[1]], c=clustering.labels_)
+            plt.colorbar()
+            fig.savefig(f"projection_{pair[0]}_{pair[1]}")
+
+    clustering.labels_.tolist()
+    cluster_lists = []
+    for cli in range(0, args.num_clusters[0]):
+        cluster_list = []
+        for docindex in np.argwhere(clustering.labels_ == cli).tolist():
+            paper = papers[docindex[0]]
+            cluster_list.append(paper)
+        print("Cluster", cli, len(cluster_list))
+        cluster_lists.append(cluster_list)
+        with open(f"cluster_{cli}_papers.json") as f:
+            f.write(cluster_list)
